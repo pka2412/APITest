7caf5ea07042eb92a8dce1847af4493bdeaf32d3
pka2412 <pka2412@gmail.com>
2020-07-24 11:49:42 +0530

    api test project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+
+const SendResquest = (event) => {
+  axios
+    .get("https://www.googleapis.com/books/v1/volumes?q=flowers")
+    .then((response) => console.log(response));
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      <button onClick={SendResquest}>Send Request</button>
     </div>
   );
 }
